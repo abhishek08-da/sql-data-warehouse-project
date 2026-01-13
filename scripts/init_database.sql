4b1e3abe0cce845e014df4f7927271c0627fac0e
abhishek08-da <abhishek.da.08@gmail.com>
2026-01-13 16:16:03 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+/*
+=============================================================
+Create Schemas
+=============================================================
+Script Purpose:
+    This script creates the bronze, silver, and gold schemas
+    inside an existing PostgreSQL database.
+
+NOTE:
+    The database was created manually using pgAdmin.
+=============================================================
+*/
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
